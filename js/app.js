f54d7a9e98a4525f2d865830a96ef63c01fa167a
Ediano Gama <eddy.tadim@gmail.com>
2018-08-06 22:44:55 -0300

    ajustes para a ideia principal

--- js/app.js
+++ js/app.js
@@ -1,11 +1,3 @@
 $(document).foundation()
-$(document).ready(function(){
-    var windowWidth = $(window).innerWidth();
-    var sliderCount = $('#slider .featured li').length;
-    var slideWidth  = (windowWidth/sliderCount); 
-     $('#slider .featured li').each(function(){
-            $(this).width(slideWidth);
-    });
-    $('#slider .nav').width(slideWidth);
-  
+$(document).ready(function(){  
 });
